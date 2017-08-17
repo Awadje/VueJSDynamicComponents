af2ff3219ef0cc2a1740de16325da9e18877dd46
Awadje <michaelawad85@gmail.com>
2017-08-17 15:27:31 +0200

    keep-alive, desroyed, activated and deactivated lifecycle hooks

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
         <h3> New Quote </h3>
+        <p> {{ counter }}</p>
+        <button @click="counter++">Increase!</button>
     </div>
 </template>
 
 <script>
     export default {
+        data: function () {
+            return {
+                counter: 0
+            };
+        },
+        destroyed() {
+            console.log('Destroyed!');
+        },
+        deactivated() {
+            console.log('Deactivated!')
+        },
+        activated() {
+            console.log('Activated!')
+        }
     }
 </script>
 
